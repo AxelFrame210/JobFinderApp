390762917914eb64260804d47b873ed4c8ae530d
AxelFrame210 <duylekhanh.jeff1002@gmail.com>
2025-03-26 21:02:31 +0700

    Initial Commit

---
+++ JobFinderApp/JobFinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobFinderAppApp.swift
+//  JobFinderApp
+//
+//  Created by Admin on 26/3/25.
+//
+
+import SwiftUI
+
+@main
+struct JobFinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
